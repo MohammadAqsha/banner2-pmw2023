fa861d2c42960344c3ebaf04d7b7a839bb2a4abd
MohammadAqsha <aqshasticker@gmail.com>
2023-11-24 20:01:22 +0800

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,71 @@
+"use client";
+
+import { useState } from 'react'
 import Image from 'next/image'
-import styles from './page.module.css'
+import '@style/home.css'
 
 export default function Home() {
+  const [isSimpan, setSimpan] = useState("");
+  const [nama, setNama] = useState("Mohammad Aqsha Arianto Latif");
+
+  function handlerInputNama(event) {
+    // Prevent the browser from reloading the page
+    event.preventDefault();
+    setSimpan(event.target.value);
+  }
+  function handlerSubmit(event) {
+    // Prevent the browser from reloading the page
+    setNama(isSimpan);
+  }
+  function handlerKeyEnter(e){
+    e.preventDefault
+    if (e.key === 'Enter'){
+      setSimpan(e.target.value)
+      setNama(isSimpan);
+    }
+  }
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
+    <>
+      <div className="banner-container">
+        {/* Kartunya */}
+        <div className="header-banner-wrapper">
+          {/* Foto Profil dan Nama */}
+          <div className="profile-header-banner">
+            {/* Foto Profil*/}
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
+              src="/assets/profil.png"
+              alt="Picture of the author"
+              fill
+              objectFit="contain"
             />
-          </a>
+          </div>
+          <div className="content-header-banner">
+            {/* Nama dan Kawan2*/}
+            <h1>{nama}</h1>
+            <div className="bio-nim-header-banner">
+              {/* NIM dan BIO*/}
+              <p> D121211018</p>
+              <p>Survey Corps, Shinzou Sasageyo</p>
+            </div>
+          </div>
+        </div>
+        <div className="cta-banner-wrapper">
+          {/* Tombol CTA */}
+          <input
+            name="input-nama"
+            type="text"
+            placeholder="Tuliskan namamu.."
+            onChange={handlerInputNama}
+            onKeyDown={handlerKeyEnter}
+          />
+          {isSimpan ? (<div className="cta-button" onClick={handlerSubmit}>
+            <p>Submit Nama</p>
+          </div>) : (<div className="cta-button disabled" onClick={()=>alert('Isi terlebih dahulu!')}>
+            <p>Disabled</p>
+          </div>)}
         </div>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    </>
+  );
 }
