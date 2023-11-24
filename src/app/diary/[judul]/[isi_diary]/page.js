d4de3616e4e04bfbd6b7bf35188556c3af798ef1
MohammadAqsha <aqshasticker@gmail.com>
2023-11-24 21:14:20 +0800

    update

--- src/app/diary/[judul]/[isi_diary]/page.js
+++ src/app/diary/[judul]/[isi_diary]/page.js
@@ -1,16 +1,12 @@
-<<<<<<< HEAD
 import "../../../../styles/diarypost.css";
-=======
-import "../../style/diary.css";
->>>>>>> 131bdd8d50d05602bdb4aa44fd7084db0bc3c609
 
-export default function DiaryPost({params}) {
-    const {judul, isi_diary} = params
+export default function DiaryPost({ params }) {
+  const { judul, isi_diary } = params;
 
-    return (
-        <div className= " diary-post-container">
-            <h1>{decodeURIComponent(judul)}</h1>
-            <p>{decodeURIComponent(isi_diary)}</p>
-        </div>
-    )
+  return (
+    <div className="diary-post-container">
+      <h1>{decodeURIComponent(judul)}</h1>
+      <p>{decodeURIComponent(isi_diary)}</p>
+    </div>
+  );
 }
